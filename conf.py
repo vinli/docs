b037dc5e2c83001776080db109be003a491c4338
Daniel T. Hall <danielthall@me.com>
2015-04-01 15:59:29 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -45,7 +45,6 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 # html_theme = []
 html_static_path = ['_static']
-html_theme = 'sphinx_rtd_theme'
 html_style = 'css/vinli.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
